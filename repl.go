590a1455cf0b77a3dd99fb3651ff605a84f15edc
Ell534 <elliot94b@gmail.com>
2023-11-17 16:18:30 +0000

    add explore, catch commands

--- repl.go
+++ repl.go
@@ -22,6 +22,10 @@ func startRepl(cfg *config) {
 		}
 
 		commandName := cleaned[0]
+		args := []string{}
+		if len(cleaned) > 1 {
+			args = cleaned[1:]
+		}
 
 		availableCommands := getCommands()
 
@@ -31,7 +35,7 @@ func startRepl(cfg *config) {
 			continue
 		}
 
-		err := command.callback(cfg)
+		err := command.callback(cfg, args...)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -41,7 +45,7 @@ func startRepl(cfg *config) {
 type cliCommand struct {
 	name        string
 	description string
-	callback    func(*config) error
+	callback    func(*config, ...string) error
 }
 
 func getCommands() map[string]cliCommand {
@@ -65,6 +69,16 @@ func getCommands() map[string]cliCommand {
 			name:        "mapb",
 			description: "Lists the previous page of location areas",
 			callback:    commandMapb,
+		},
+		"explore": {
+			name:        "explore {location_area}",
+			description: "Lists the pokemon in a location area",
+			callback:    commandExplore,
+		},
+		"catch": {
+			name:        "catch {pokemon_name}",
+			description: "Attempts to catch the name pokemon",
+			callback:    commandCatch,
 		},
 	}
 }
