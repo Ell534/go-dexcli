5ef47dfaa35e48acc6ef4461bf298088eb777deb
Ell534 <elliot94b@gmail.com>
2024-02-23 13:53:16 +0000

    add channel to receive syscall signals to exit program to fix writing to syslog issue

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/Ell534/go-dexcli/internal/pokeapi"
@@ -10,7 +14,7 @@ type config struct {
 	pokeapiClient       pokeapi.Client
 	nextLocationAreaURL *string
 	prevLocationAreaURL *string
-	caughtPokemon map[string]pokeapi.Pokemon
+	caughtPokemon       map[string]pokeapi.Pokemon
 }
 
 func main() {
@@ -19,5 +23,19 @@ func main() {
 		caughtPokemon: make(map[string]pokeapi.Pokemon),
 	}
 
-	startRepl(&cfg)
+	// Set up channel to listen for interrupt and hang-up signals
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+
+	// Run the REPL in a separate goroutine so that signal handling can proceed concurrently
+	go startRepl(&cfg)
+
+	// Block until a signal is received
+	sig := <-sigChan
+
+	// Perform any cleanup if necessary
+	fmt.Printf("\nReceived %v signal. Exiting pokedex...\n", sig)
+
+	// Exit the program
+	os.Exit(0)
 }
